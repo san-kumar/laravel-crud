e67c4b83406e32ae6182e89503e118c31daf4dc5
Gerardp <gporto@gmail.com>
2023-01-19 14:20:22 +0100

    Bugfix radio-button

--- src/template/stubs/views/bootstrap/partials/inputs/boolean.blade.php
+++ src/template/stubs/views/bootstrap/partials/inputs/boolean.blade.php
@@ -1,10 +1,10 @@
 <div>
     <div class="form-check form-check-inline">
         <input class="form-check-input" type="radio" name="_id_" id="_id__yes" value="1" @checked((_val_) == '1') _required_>
-        <label class="form-check-label" for="inlineRadio1">Yes</label>
+        <label class="form-check-label" for="_id__yes"">Yes</label>
     </div>
     <div class="form-check form-check-inline">
         <input class="form-check-input" type="radio" name="_id_" id="_id__no" value="0" @checked((_val_) == '0') _required_>
-        <label class="form-check-label" for="inlineRadio2">No</label>
+        <label class="form-check-label" for="_id__no"">No</label>
     </div>
 </div>
